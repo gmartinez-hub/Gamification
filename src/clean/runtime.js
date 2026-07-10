0cb27abfc6ffad6cb5d7b3e1bb446e86641b349c
gmartinez-hub <g.martinez@paisanos.io>
2026-07-10 14:34:28 -0300

    Fix clean runtime Three import

--- src/clean/runtime.js
+++ src/clean/runtime.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "../vendor/three.module.js";
 import "./styles.css";
 import { AudioDirector } from "./audio.js";
 import { UI } from "./ui.js";
